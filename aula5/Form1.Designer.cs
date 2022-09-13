7e99d34e21f5aa599746cfbd0add71508b962bfd
Administrador <Administrador@LAB06-29>
2022-09-12 22:12:15 -0300

    Adicionar arquivos de projeto.

---
+++ aula5/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace aula5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPais = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.Gravar = new System.Windows.Forms.Button();
+            this.Pesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(122, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(336, 20);
+            this.txtNome.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "País:";
+            // 
+            // txtPais
+            // 
+            this.txtPais.Location = new System.Drawing.Point(122, 102);
+            this.txtPais.Name = "txtPais";
+            this.txtPais.Size = new System.Drawing.Size(336, 20);
+            this.txtPais.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Estado:";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Location = new System.Drawing.Point(122, 152);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(336, 20);
+            this.txtEstado.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(33, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Titulo: ";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(122, 207);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(336, 20);
+            this.txtTitulo.TabIndex = 6;
+            // 
+            // Gravar
+            // 
+            this.Gravar.Location = new System.Drawing.Point(77, 260);
+            this.Gravar.Name = "Gravar";
+            this.Gravar.Size = new System.Drawing.Size(115, 41);
+            this.Gravar.TabIndex = 8;
+            this.Gravar.Text = "Gravar";
+            this.Gravar.UseVisualStyleBackColor = true;
+            this.Gravar.Click += new System.EventHandler(this.Gravar_Click);
+            // 
+            // Pesquisar
+            // 
+            this.Pesquisar.Location = new System.Drawing.Point(238, 260);
+            this.Pesquisar.Name = "Pesquisar";
+            this.Pesquisar.Size = new System.Drawing.Size(134, 41);
+            this.Pesquisar.TabIndex = 9;
+            this.Pesquisar.Text = "Pesquisar";
+            this.Pesquisar.UseVisualStyleBackColor = true;
+            this.Pesquisar.Click += new System.EventHandler(this.Pesquisar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 357);
+            this.Controls.Add(this.Pesquisar);
+            this.Controls.Add(this.Gravar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEstado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPais);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPais;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEstado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Button Gravar;
+        private System.Windows.Forms.Button Pesquisar;
+    }
+}
+
---
+++ aula5/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aula5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Time obj = new Time();
+        Time[] v = new Time[10];
+        int i = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Time timinho = new Time("Brasil","SP");
+            //MessageBox.Show("Nome: " + timinho.Nome + "\nPaís: " + timinho.Pais + "\nEstado: "+timinho.Estado+"\nTítulos: "+timinho.Titulo);
+        }
+
+        private void Gravar_Click(object sender, EventArgs e)
+        {
+            if (i < 10)
+            {
+                obj.Nome = txtNome.Text;
+                obj.Pais = txtPais.Text;
+                obj.Estado = txtEstado.Text;
+                obj.Titulo = Convert.ToInt32(txtTitulo.Text);
+                v[i] = obj;
+                i++;
+            }
+            else
+            {
+                MessageBox.Show("Não é possível gravar!");
+            }
+        }
+
+        private void Pesquisar_Click(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (v[j].Equals(txtNome.Text))
+                {
+                    txtEstado.Text = v[j].Estado;
+                    txtPais.Text = v[j].Pais;
+                    txtTitulo.Text = v[j].Titulo.ToString();
+                    j = 9;
+                }
+                else
+                {
+                    if (j == 9) MessageBox.Show("Time não encontrado!");
+                }
+            }
+        }
+    }
+}
